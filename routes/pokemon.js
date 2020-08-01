569854052604e9898af49371f1900b032559188b
JasonWandragLCA <jason@lifechoices.co.za>
2020-08-01 23:07:51 +0200

    added pokemon get route

--- routes/pokemon.js
+++ routes/pokemon.js
@@ -1,11 +1,24 @@
 const express = require("express");
 const router = express.Router();
+const auth = require("../middleware/auth");
+const { check, validationResult } = require("express-validator");
+// Import Models
+const User = require("../models/User");
+const Pokemon = require("../models/Pokemon");
 
 // @route   GET api/pokemon
 // @desc    Get all users pokemon
 // @access  Private
-router.get("/", (req, res) => {
-  res.send("Get all pokemon");
+router.get("/", auth, async (req, res) => {
+  try {
+    const pokemon = await Pokemon.find({ user: req.user.id }).sort({
+      date: -1,
+    });
+    res.json(pokemon);
+  } catch (err) {
+    console.error(err.message);
+    res.status(500).send("Server Error");
+  }
 });
 
 // @route   POST api/pokemon
